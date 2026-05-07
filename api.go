729f8c3c0a1e137a0b365317fa78f88868a67eb8
newTurbo50053 <sochan.tomek04@gmail.com>
2026-05-08 01:44:57 +0200

    Aplha Vantage global quote response

--- api.go
+++ api.go
@@ -1,68 +1,84 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
 )
 
-type Stock struct {
-	Symbol string  `json:"Symbol"`
-	Price  float64 `json:"Price"`
-	Volume int     `json:"Volume"`
+type GlobalQuoteResponse struct {
+	GlobalQuote Quote `json:"Global Quote"`
 }
 
-func priceChange(stock Stock, newPrice float64, oldPrice float64) {
-	change := (newPrice - oldPrice) / oldPrice * 100
-
-	fmt.Printf("Price change in percentage for: %v is %.2f%% \n", stock.Symbol, change)
+type Quote struct {
+	Symbol string `json:"01. symbol"`
+	Price  string `json:"05. price"`
+	Volume string `json:"06. volume"`
 }
 
-func mostValuableStock(stocks []Stock) (string, float64) {
+// func priceChange(stock Stock, newPrice float64, oldPrice float64) {
+// 	change := (newPrice - oldPrice) / oldPrice * 100
 
-	maxNazwa := stocks[0].Symbol
-	maxPrice := stocks[0].Price
+// 	fmt.Printf("Price change in percentage for: %v is %.2f%% \n", stock.Symbol, change)
+// }
 
-	for _, s := range stocks {
-		if s.Price > maxPrice {
-			maxPrice = s.Price
-			maxNazwa = s.Symbol
+// func mostValuableStock(stocks []Stock) (string, float64) {
 
-		}
-	}
-	return maxNazwa, maxPrice
+// 	maxNazwa := stocks[0].Symbol
+// 	maxPrice := stocks[0].Price
 
-}
+// 	for _, s := range stocks {
+// 		if s.Price > maxPrice {
+// 			maxPrice = s.Price
+// 			maxNazwa = s.Symbol
 
-func averagePrice(stocks []Stock) float64 {
-	var price float64
-	for _, s := range stocks {
-		price += (s.Price)
-	}
-	var avgPrice float64 = price / float64(len(stocks))
-	return avgPrice
-}
+// 		}
+// 	}
+// 	return maxNazwa, maxPrice
+
+// }
+
+// func averagePrice(stocks []Stock) float64 {
+// 	var price float64
+// 	for _, s := range stocks {
+// 		price += (s.Price)
+// 	}
+// 	return price / float64(len(stocks))
+// }
 
 func api() {
+
+	var response GlobalQuoteResponse
+
 	apiKey := os.Getenv("ALPHA_API_KEY")
 
 	if apiKey == "" {
-		fmt.Println("API KEY not found bitch ass ")
+		fmt.Println("API KEY not found")
 		return
 	}
 
 	symbol := "IBM"
-
-	resp, err := http.Get("https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + symbol + "&apikey=" + apiKey)
+	url := "https://www.alphavantage.co/query?function=GLOBAL_QUOTE&symbol=" + symbol + "&apikey=" + apiKey
+	resp, err := http.Get(url)
 
 	if err != nil {
-		fmt.Println("PROBLRM")
+		fmt.Println("PROBLRM: ", err)
+		return
 	}
 
 	defer resp.Body.Close()
 
-	bodyBytes, _ := io.ReadAll(resp.Body)
+	bodyBytes, err := io.ReadAll(resp.Body)
+
+	err = json.Unmarshal(bodyBytes, &response)
+
+	if err != nil {
+		fmt.Println("PROBLEM:", err)
+		return
+	}
 	fmt.Println(string(bodyBytes))
+	fmt.Printf("Symbol: %s\nCena : %v", response.GlobalQuote.Symbol, response.GlobalQuote.Price)
 
 }
